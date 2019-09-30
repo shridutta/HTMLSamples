c426f6592c9f737ad6c348bf8a1ac479985c68fe
SHRI PACHORI <shridutta@hotmail.com>
2019-09-30 23:41:21 +0530

    Added Db features

--- API.AgriFood/app.js
+++ API.AgriFood/app.js
@@ -5,8 +5,38 @@ const port = 9000;
 var router = express.Router();
 var bodyParser  = require('body-parser');
 var cookieParser = require('cookie-parser');
+var mysql = require('mysql'), async = require('async')
 
+var PRODUCTION_DB = 'app_prod_database'
+  , TEST_DB = 'app_test_database'
 
+exports.MODE_TEST = 'mode_test'
+exports.MODE_PRODUCTION = 'mode_production'
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_user',
+  password: 'some_secret',
+  database: 'the_app_database'
+});
+
+var state = {
+   pool: null,
+   mode: null,
+ }
+ 
+ exports.connect = function(mode, done) {
+   state.pool = mysql.createPool({
+     host: 'localhost',
+     user: 'your_user',
+     password: 'some_secret',
+     database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
+   })
+ 
+   state.mode = mode
+   done()
+ }
+ 
 app.use(cookieParser())
 
 //First middleware before response is sent
@@ -47,3 +77,20 @@ app.use('/', function(req, res){
    console.log('End');
 });
 
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'dbuser',
+//   password: 's3kreee7',
+//   database: 'my_db'
+// })
+
+// connection.connect()
+
+// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+//   if (err) throw err
+
+//   console.log('The solution is: ', rows[0].solution)
+// })
+
+// connection.end()
