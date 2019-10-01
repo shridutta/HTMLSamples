d8d6e3f690e27d7d52fb2f9bbbb33a97dd357a89
SHRI PACHORI <shridutta@hotmail.com>
2019-10-01 11:03:23 +0530

    Fixed errors

--- API.AgriFood/app.js
+++ API.AgriFood/app.js
@@ -4,53 +4,6 @@ const app = express();
 const port = 9000;
 var router = express.Router();
 var bodyParser  = require('body-parser');
-var cookieParser = require('cookie-parser');
-var mysql = require('mysql'), async = require('async')
-
-var PRODUCTION_DB = 'app_prod_database'
-  , TEST_DB = 'app_test_database'
-
-exports.MODE_TEST = 'mode_test'
-exports.MODE_PRODUCTION = 'mode_production'
-
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'your_user',
-  password: 'some_secret',
-  database: 'the_app_database'
-});
-
-var state = {
-   pool: null,
-   mode: null,
- }
- 
- exports.connect = function(mode, done) {
-   state.pool = mysql.createPool({
-     host: 'localhost',
-     user: 'your_user',
-     password: 'some_secret',
-     database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
-   })
- 
-   state.mode = mode
-   done()
- }
- 
-app.use(cookieParser())
-
-//First middleware before response is sent
-app.use(function(req, res, next){
-   console.log("Start");
-   next();
-});
-
-//To parse URL encoded data
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//To parse json data
-app.use(bodyParser.json())
-
 app.get('/login', function(req, res){
    res.send("Please provide username and password! ");
 });
