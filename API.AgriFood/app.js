5121a3d99d3d3cb50d9aa6a2905f4269351368d9
d4i <deeptipatel1416@gmail.com>
2020-01-05 19:54:57 +0530

    deepti init

--- API.AgriFood/app.js
+++ API.AgriFood/app.js
@@ -1,83 +1,92 @@
-
 const express = require('express');
 const app = express();
 const port = 9000;
 var cors = require('cors');
 var router = express.Router();
-var bodyParser  = require('body-parser');
-var mysql      = require('mysql');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+var  mysql       =  require('mysql');
+var  bodyParser  =  require('body-parser');
+const path = require('path');
 
 //start mysql connection
-var connection = mysql.createConnection({
-  host     : 'localhost', //mysql database host name
-  user     : 'root', //mysql database user name
-  password : 'Welcome123', //mysql database password
-  database : 'AgriFoods' //mysql database name
+var  connection  =  mysql.createConnection({  
+    host     :   'localhost',
+      //mysql database host name
+      user     :   'root',
+      //mysql database user name
+      password :   '1234',
+      //mysql database password
+      database :   'AgriFoods'  //mysql database name
 });
 
 
-connection.connect(function(err) {
-     if (err) throw err
-     console.log('You are now connected with mysql database...')
-   });
+connection.connect(function(err)  {  
+    if  (err)  throw  err ; 
+    console.log('You are now connected with mysql database...')
+});
 
 //start body-parser configuration
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+app.use( bodyParser.json() );        // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({      // to support URL-encoded bodies
+      
+    extended:  true
 }));
 //end body-parser configuration
 
 //create app server
-var server = app.listen(port,  "127.0.0.1", function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
+var  server  =  app.listen(port,   "127.0.0.1",  function ()  {  
+    var  host  =  server.address().address  
+    var  port  =  server.address().port
+    console.log("Example app listening at http://%s:%s",  host,  port)
 });
 
-app.get('/', function(req, res){
-   res.send("Welcome to AgriFoods ");
+app.get('/use', function(req, res) {
+    //res.send(path.join(__dirname, '../AgriFoods/login.html'));
+
+    //console.log(a);
+
+    //res.send("hello");
+    res.sendFile(path.join(__dirname, '../AgriFoods/login.html'));
+
 });
 
-app.get('/login', function(req, res){
-   res.send("Please provide username and password! ");
+app.get('/login', function(req, res) {
+    res.send("Please provide username and password! ");
 });
 
 
-app.get('/login/:userName/:pwd', function(req, res){
-   // res.send("Welcome "+req.params.userName+" Your password is "
-   // +req.params.pwd
-   // );
-   var query = 'select * from users where userName=\''+req.params.userName + '\' AND PASSWORD = \'' +req.params.pwd +'\' AND isActive =1';
-   console.log(query);
-   connection.query(query, function (error, results, fields) {
-         if (error ) 
-         {
+app.get('/login/:userName/:pwd', function(req, res) {
+    // res.send("Welcome "+req.params.userName+" Your password is "
+    // +req.params.pwd
+    // );
+    var query = 'select * from users where userName=\'' + req.params.userName + '\' AND PASSWORD = \'' + req.params.pwd + '\' AND isActive =1';
+    console.log(query);
+    connection.query(query,  function (error,  results,  fields)  {     
+        if  (error)  {
             console.log(error);
-            throw error;
-         }
-         console.log(results);
-         if(results.length == 0)
-              res.end(JSON.stringify('Invalid userName or Pwd'));
-            else
-             res.end(JSON.stringify(results));
-   });
+            throw  error;
+        }
+        console.log(results);
+        if (results.length == 0)
+            res.end(JSON.stringify('Invalid userName or Pwd'));
+        else
+            res.end(JSON.stringify(results));
+    });
 
-      
-   //TODO: Get the Record from DB 
-   // If found say welcome
-   //else say invalid un pwd
+
+    //TODO: Get the Record from DB 
+    // If found say welcome
+    //else say invalid un pwd
 
 });
 
 //Other routes here
-app.get('*', function(req, res){
-   res.send('Sorry, please check the URL.');
+app.get('*', function(req, res) {
+    res.send('Sorry, please check the URL.');
 });
 
-app.use('/', function(req, res){
-   console.log('End');
+app.use('/', function(req, res) {
+    console.log('welcome');
 });
 
 // var mysql = require('mysql')
--- AgriFoods/assets/scripts/main.js
+++ AgriFoods/assets/scripts/main.js
@@ -3,12 +3,12 @@
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
-  } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
-  }
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
 }
 
 // Personalized welcome message code
@@ -17,22 +17,22 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName || myName === null) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-  }
+    let myName = prompt('Please enter your name.');
+    if (!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
 }
 
-if(!localStorage.getItem('name')) {
-  setUserName();
+if (!localStorage.getItem('name')) {
+    setUserName();
 } else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
-  setUserName();
+    setUserName();
 }
